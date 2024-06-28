fcb65b913cd42b3fe932ac756fbdfa16326e2878
Evert van Noort <evert@evertvannoort.com>
2024-06-28 22:19:35 +0200

    testNewPage
    to replace the hardcoded keys

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    emailjsUserId: "VfQnHi5XfW-5T-VNp",
+    emailjsServiceId: "service_m47km58",
+    emailjsTemplateId: "template_8fuz2bb",
+    toName: "Evert van Noort",
+    fromName: "Website",
+    message: "Someone tried to use the service but the server is down."
+};
+
+export default config;
